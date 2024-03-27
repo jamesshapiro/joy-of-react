590d89d22e96b4562dd3ed7732ad6940aef46d07
James Shapiro <jamesshapiro@gmail.com>
2024-03-27 02:39:57 -0400

    first pass solution

---
+++ 06-next-exercises/05-next-ssr-gotchas/src/components/Counter/Counter.js
@@ -0,0 +1,29 @@
+'use client';
+import React from 'react';
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    const savedValue = window.localStorage.getItem('saved-count');
+    if (savedValue === null) {
+      return
+    }
+    setCount(Number(savedValue))
+  }, [])
+
+  React.useEffect(() => {
+    window.localStorage.setItem('saved-count', count);
+  }, [count]);
+
+  return (
+    <button
+      className="count-btn"
+      onClick={() => setCount(count + 1)}
+    >
+      Count: {count}
+    </button>
+  );
+}
+
+export default Counter;
